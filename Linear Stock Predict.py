ce98597ce663ce4bc937735cfe4924169c0f5de3
VCK0395 <87344807+VCK0395@users.noreply.github.com>
2021-08-28 05:00:34 -0500

    Add files via upload

---
+++ Linear Stock Predict.py
@@ -0,0 +1,77 @@
+# Import Libraries
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.svm import SVR
+from sklearn.ensemble import RandomForestRegressor
+
+
+
+
+
+
+# Load Amazon's Stock Data
+price = pd.read_csv('AMZN.csv')
+last_price = price.tail(1)
+
+# Drop the last Day
+price = price.head(len(price)-1)
+
+
+days = list()
+adjusted_price = list()
+
+# Picking the Data adn Adjusted Close Price
+price_days = price.loc[:, "Date"]
+price_adj = price.loc[:, "Adj Close"]
+print(price_adj)
+
+"""
+# Append to a List
+for D in price_days:
+    days.append(D.split('-')[2])
+
+for ADJ in price_adj:
+    adjusted_price.append(float(ADJ))
+
+# Prepare the Models
+X = np.array(days).reshape(-1, 1)
+y = adjusted_price
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# SVR Model
+SVR_model = SVR(kernel='rbf', C=1000, gamma=0.85)
+SVR_model.fit(X_train, y_train)
+
+# Linear Regression Model
+Lin_model = LinearRegression()
+Lin_model.fit(X_train, y_train)
+
+# RandomForestRegressor Model
+RFR_model = RandomForestRegressor()
+RFR_model.fit(X_train, y_train)
+
+
+# Test the Prediction 
+last_day = [[31]]
+
+
+print('LinearRegression Predicted Price =', Lin_model.predict(last_day))
+print('SVR_rgf Predicted Price =', SVR_model.predict(last_day))
+print('RFR Predicted Price =', RFR_model.predict(last_day))
+print('Amazon Actual Price =', last_price["Adj Close"][19])
+"""
+
+
+
+
+
+
+
+
